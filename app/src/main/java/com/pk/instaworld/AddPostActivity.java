1efa3f021f9047ace7f72e81163b5c60f6021cb5
Phyo Khine <pkhine.2020@berkeley.edu>
2019-11-12 23:24:59 -0800

    Having fun

--- app/src/main/java/com/pk/instaworld/AddPostActivity.java
+++ app/src/main/java/com/pk/instaworld/AddPostActivity.java
@@ -1,14 +1,118 @@
 package com.pk.instaworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.parceler.Parcels;
+
+import java.io.File;
 
 public class AddPostActivity extends AppCompatActivity {
 
+
+    ImageView ivPostToBe;
+    Button btnTakePictureAgain;
+    Button btnSharePost;
+    Button btnCancelPost;
+    Button btnEditPhoto;
+    TextView etDescription;
+
+    public static final String TAG = "AddPostActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+        Log.i(TAG, "Inside AddPostActivity");
+        ivPostToBe = findViewById(R.id.ivPostToBe);
+        btnTakePictureAgain = findViewById(R.id.btnTakePictureAgain);
+        btnSharePost = findViewById(R.id.btnSharePost);
+        btnCancelPost = findViewById(R.id.btnCancelPost);
+        btnEditPhoto = findViewById(R.id.btnEditPhoto);
+        etDescription = findViewById(R.id.etDescription);
+
+
+
+        Bitmap image = getIntent().getParcelableExtra("picture");
+        ivPostToBe.setImageBitmap(image);
+
+        btnCancelPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(AddPostActivity.this, MainActivity.class);
+                setResult(RESULT_OK, i);
+
+            }
+        });
+
+        btnSharePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // save post and description
+                String description = etDescription.getText().toString();
+                ParseUser user = ParseUser.getCurrentUser();
+                savePost(description, user, (File) getIntent().getSerializableExtra("pictureFile"));
+                Intent i = new Intent(AddPostActivity.this, MainActivity.class);
+                setResult(RESULT_OK, i);
+                finish();
+                // go to MainActivity
+            }
+        });
+
+        btnTakePictureAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to camera again
+                Toast.makeText(AddPostActivity.this, "Add More Not Yet Implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEditPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AddPostActivity.this, "Edit Button Not Yet Implemented", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    private void savePost(String description, ParseUser parseUser, File pictureFile) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(parseUser);
+        post.setImage(new ParseFile(pictureFile));
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.d(TAG, "Error while saving post");
+                    e.printStackTrace();
+                    return;
+                }
+                Log.d(TAG, "Successfully Saved Post!");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/pk/instaworld/MainActivity.java
+++ app/src/main/java/com/pk/instaworld/MainActivity.java
@@ -1,18 +1,37 @@
 package com.pk.instaworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcel;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.parceler.Parcels;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
-    Button btnAddPost;
+    Button btnTakePicture;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public static final int ADD_POST_REQUEST_CODE = 20;
+
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +40,90 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        btnAddPost = findViewById(R.id.btnAddPost);
+        btnTakePicture = findViewById(R.id.btnTakePicture);
 
-        btnAddPost.setOnClickListener(new View.OnClickListener() {
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), AddPostActivity.class);
-                startActivity(i);
+                launchCamera(view);
             }
         });
+    }
+
+    private void launchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+//                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // Load a bitmap from the drawable folder
+
+                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+                // by this point we have the camera photo on disk
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+
+                // Resize the bitmap to 150x100 (width x height)
+                Bitmap bMapScaled = Bitmap.createScaledBitmap(rawTakenImage, 50, 50, true);
+
+
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                Intent i = new Intent(MainActivity.this, AddPostActivity.class);
+                i.putExtra("picture", bMapScaled);
+                i.putExtra("pictureFile", photoFile);
+                startActivityForResult(i, ADD_POST_REQUEST_CODE);
+
+                // Pass it to AddPostActivity with Parcelable
+//                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+//                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        } else if(requestCode == ADD_POST_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Use Recycler view to display all the posts
+            Log.i(TAG, "Added a post successfully!");
+        }
+
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 }
